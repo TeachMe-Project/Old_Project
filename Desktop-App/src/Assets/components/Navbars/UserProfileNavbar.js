c38c63dd8d706d7540e3dd0afd1a8fbc4f7929b5
Maneth Wijetunga <manethwijetunga@Maneths-MacBook-Air.local>
2022-04-29 19:19:55 +0530

    User profile for student 1.0

--- Desktop-App/src/Assets/components/Navbars/UserProfileNavbar.js
+++ Desktop-App/src/Assets/components/Navbars/UserProfileNavbar.js
@@ -1,16 +1,24 @@
-import React from 'react'
+import React from "react";
 import { BsFillBellFill } from "react-icons/bs";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const UserProfileNavbar = (props) => {
   return (
-    <div className='UserProfileNavbar'>
-        {/* <Link to ='#'> */}
-        <div className="UserImg">  <img src={require("../../Images/testimg2.jpeg")} /></div>
-        <div className="Name"> {props.children}</div>
-        <div className="NotificationBell"> <BsFillBellFill className="Bell-icon" /></div>
-        {/* </Link>  */}
+    <div className="UserProfileNavbar">
+      <div className="UserProfileButton">
+        <Link to="/userprofile" className="link">
+          <div className="UserImg">
+            <img src={require("../../Images/testimg2.jpeg")} />
+          </div>
+          <div className="Name"> {props.children}</div>
+        </Link>
+      </div>
+      <Link to="#">
+        <div className="NotificationBell">
+          <BsFillBellFill className="Bell-icon" />
         </div>
-  )
-}
+      </Link>
+    </div>
+  );
+};
 export default UserProfileNavbar;
