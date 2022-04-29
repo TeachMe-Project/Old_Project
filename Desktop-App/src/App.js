64e14d1605627ca8eb25eb8599be5226026d1524
hbsbashitha <hbsbashitha@gmail.com>
2022-04-29 12:42:38 +0530

    change file paths and gitignore

--- Desktop-App/src/App.js
+++ Desktop-App/src/App.js
@@ -1,20 +1,26 @@
-import '../src/Assets/Styles/main.scss';
-import LeftSidebar from '../src/Assets/components/Sidebar/LeftSidebar';
-import RightSidebar from '../src/Assets/components/Sidebar/RightSidebar';
-import MainPanel from '../src/Assets/components/Pages/Student/MainPanel';
-import { Row, Col } from 'react-bootstrap';
-import {BrowserRouter as Router} from "react-router-dom";
+import "./assets/Styles/main.scss";
+import LeftSidebar from "./assets/components/Sidebar/LeftSidebar";
+import RightSidebar from "./assets/components/Sidebar/RightSidebar";
+import MainPanel from "./assets/components/Pages/Student/MainPanel";
+import { Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">  
+      <div className="App">
         <Row className="Page">
-          <Col xl={2}><LeftSidebar/></Col>
-          <Col xl={7}><MainPanel/></Col>
-          <Col xl={3}><RightSidebar/></Col>
+          <Col xl={2}>
+            <LeftSidebar />
+          </Col>
+          <Col xl={7}>
+            <MainPanel />
+          </Col>
+          <Col xl={3}>
+            <RightSidebar />
+          </Col>
         </Row>
-    </div>
+      </div>
     </Router>
   );
 }
