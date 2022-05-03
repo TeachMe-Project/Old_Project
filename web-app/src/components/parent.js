4e897e754dd8aac53ca03966f68f0b7ebe7bd503
jonathan <jonathan10dass@gmail.com>
2022-05-03 20:27:25 +0530

    setting up parent ui

---
+++ web-app/src/components/parent.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+// const Parent = () => {
+//   return (
+//     <h1>Parent</h1>
+//   );
+// }
+class Parent extends Component {
+  render() {
+    return (
+      <h1>Parent</h1>
+    );
+  }
+}
+
+
+export default Parent;
