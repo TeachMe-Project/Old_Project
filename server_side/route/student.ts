e57f6f718279392f7f77c6dcc12d9c7323ddb0e7
Prasadkpd <kpdplakshan@gmail.com>
2022-05-13 21:03:23 +0530

    some part done

---
+++ server_side/route/student.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import controller from '../controllers/student';
+
+const router = express.Router();
+
+router.get('/get', controller.getAll);
+
+export = router;
--- server_side/src/server.ts
+++ server_side/src/server.ts
@@ -1,8 +1,54 @@
 import {Express} from "express";
 import express = require("express");
+import config from "../config/config";
+import logging from "../utils/logger";
+import bodyParser from "body-parser";
+import studentRoutes from '../route/student'
 
+const port = config.server.port;
 const app: Express =  express();
+const NAMESPACE = 'Server';
 
-app.listen(1337, ()=> {
-    console.log("App is running")
-})
+/** Log the request */
+app.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        /** Log the res */
+        logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    })
+
+    next();
+});
+
+/** Parse the body of the request */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+/** Rules of our API */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+
+app.use('/users', studentRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+
+app.listen(config.server.port, () => logging.info(NAMESPACE, `Server is running ${config.server.hostname}:${config.server.port}`));
