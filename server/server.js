a78d0f48b16cf24b010c191bec2dff70838fa579
Prasadkpd <kpdplakshan@gmail.com>
2022-05-26 11:38:59 +0530

    add server

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const app = require('./app');
+const config = require('./config/config');
+const logging = require('./utils/logger');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const host = config.server.hostname;
+const port = config.server.port;
+const NAMESPACE = 'Server';
+
+/** Log the request */
+app.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+    res.on('finish', () => {
+        /** Log the res */
+        logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${res.statusCode}] - IP: [${req.socket.remoteAddress}]`);
+    });
+
+    next();
+});
+
+/** Parse the body of the request */
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+/** Rules of our API */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+app.use((req, res) => {
+    const error = new Error('Not found');
+
+    res.status(404).json({
+        message: error.message
+    });
+});
+
+const server = http.createServer(app);
+server.listen(port, () => logging.info(NAMESPACE, `Server is running http://${host}:${port}`));
